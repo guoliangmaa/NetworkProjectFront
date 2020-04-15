fd9762ac44bbe9639d1f5287379099b069555d7f
ysuMgl <772399887@qq.com>
2020-04-16 00:46:32 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');//引入代理模块
+const app = express();
+const session = require('express-session');
+
+app.use(express.static('Project'));
+
+
+const apiProxy = proxy('/', { target: 'http://localhost:8080',changeOrigin: true })
+app.use('/', apiProxy);
+
+
+app.listen(80, () => {
+    console.log('Listening on: http://localhost:80');
+});
